b7f2b95b764f4f5981b8624b21453649d5a86927
Mohit Rathee <mohitrathee051@gmail.com>
2024-01-16 23:12:03 +0530

    n1

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import Navbar from './components/Navbar';
 import ContentSection from './components/ContentSection';
+import TaskList from './components/TaskList';
 const App = () => {
   return (
     <div>
       <Navbar />
       <ContentSection />
+      <TaskList />
     </div>
   );
 };
